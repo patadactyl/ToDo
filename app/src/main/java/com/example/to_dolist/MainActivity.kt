c9b7f9eea82923b132519115df02b4da8f3a0525
Patrick Charlton <patrick.charlton6@gmail.com>
2021-05-11 01:56:53 -0700

    Added Model for To-Do List items. Added Constants object for Firebase items. Added Adapter class for writing to Firebase. Connected app to write to real-time database.

--- app/src/main/java/com/example/to_dolist/MainActivity.kt
+++ app/src/main/java/com/example/to_dolist/MainActivity.kt
@@ -3,23 +3,33 @@ package com.example.to_dolist
 import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
+import android.provider.SyncStateContract
 import android.util.Log
-import android.util.SparseBooleanArray
-import android.widget.ArrayAdapter
+import android.view.View
+import android.widget.EditText
 import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import com.example.to_dolist.databinding.ActivityMainBinding
 import com.firebase.ui.auth.AuthUI
 import com.firebase.ui.auth.IdpResponse
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.google.firebase.auth.EmailAuthProvider
 import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.auth.ktx.auth
-import com.google.firebase.ktx.Firebase
 import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ktx.database
+import com.google.firebase.ktx.Firebase
 import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity() {
 
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     private lateinit var auth: FirebaseAuth
+    private lateinit var mDatabase: DatabaseReference
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -27,7 +37,36 @@ class MainActivity : AppCompatActivity() {
 
         auth = Firebase.auth
         createSignInIntent()
-        addToDo()
+
+        val add_btn = findViewById<View>(R.id.add_btn) as FloatingActionButton
+
+        add_btn.setOnClickListener { view ->
+            //Show Dialog here to add new Item
+            addNewItemDialog()
+        }
+
+        mDatabase = FirebaseDatabase.getInstance().reference
+    }
+
+    private fun addNewItemDialog() {
+        val alert = AlertDialog.Builder(this)
+        val itemEditText = EditText(this)
+        alert.setMessage("Add New Item")
+        alert.setTitle("Enter To Do Item Text")
+        alert.setView(itemEditText)
+        alert.setPositiveButton("Submit") { dialog, positiveButton ->
+            val todoItem = ToDoModel.create()
+            todoItem.itemText = itemEditText.text.toString()
+            todoItem.done = false
+            //We first make a push so that a new item is made with a unique ID
+            val newItem = mDatabase.child(Constants.FIREBASE_ITEM).push()
+            todoItem.objectId = newItem.key
+            //then, we used the reference to set the value on that ID
+            newItem.setValue(todoItem)
+            dialog.dismiss()
+            Toast.makeText(this, "Item saved with ID " + todoItem.objectId, Toast.LENGTH_SHORT).show()
+        }
+        alert.show()
     }
 
     public override fun onStart() {
@@ -37,6 +76,15 @@ class MainActivity : AppCompatActivity() {
         if(currentUser != null){
             reload();
         }
+    }
+
+    fun basicReadWrite() {
+        // [START write_message]
+        // Write a message to the database
+        val database = Firebase.database
+        val myRef = database.getReference("message")
+
+        myRef.setValue("Hello, World!")
     }
 
     private fun signInAnonymously() {
@@ -51,8 +99,10 @@ class MainActivity : AppCompatActivity() {
                 } else {
                     // If sign in fails, display a message to the user.
                     Log.w(TAG, "signInAnonymously:failure", task.exception)
-                    Toast.makeText(baseContext, "Authentication failed.",
-                        Toast.LENGTH_SHORT).show()
+                    Toast.makeText(
+                        baseContext, "Authentication failed.",
+                        Toast.LENGTH_SHORT
+                    ).show()
                     updateUI(null)
                 }
             }
@@ -71,8 +121,10 @@ class MainActivity : AppCompatActivity() {
                     updateUI(user)
                 } else {
                     Log.w(TAG, "linkWithCredential:failure", task.exception)
-                    Toast.makeText(baseContext, "Authentication failed.",
-                        Toast.LENGTH_SHORT).show()
+                    Toast.makeText(
+                        baseContext, "Authentication failed.",
+                        Toast.LENGTH_SHORT
+                    ).show()
                     updateUI(null)
                 }
             }
@@ -84,7 +136,8 @@ class MainActivity : AppCompatActivity() {
         // Choose authentication providers
         val providers = arrayListOf(
             AuthUI.IdpConfig.EmailBuilder().build(),
-            AuthUI.IdpConfig.GoogleBuilder().build())
+            AuthUI.IdpConfig.GoogleBuilder().build()
+        )
 
         // Create and launch sign-in intent
         startActivityForResult(
@@ -92,7 +145,8 @@ class MainActivity : AppCompatActivity() {
                 .createSignInIntentBuilder()
                 .setAvailableProviders(providers)
                 .build(),
-            RC_SIGN_IN)
+            RC_SIGN_IN
+        )
         // [END auth_fui_create_intent]
     }
 
@@ -115,50 +169,6 @@ class MainActivity : AppCompatActivity() {
             }
         }
     }
-
-    private fun addToDo () {
-        // Initializing the array lists and the adapter
-        var itemlist = arrayListOf<String>()
-        var adapter =ArrayAdapter<String>(this,
-            android.R.layout.simple_list_item_multiple_choice
-            , itemlist)
-        // Adding the items to the list when the add button is pressed
-        add.setOnClickListener {
-
-            itemlist.add(editText.text.toString())
-            listView.adapter =  adapter
-            adapter.notifyDataSetChanged()
-            // This is because every time when you add the item the input space or the eidt text space will be cleared
-            editText.text.clear()
-        }
-        // Clearing all the items in the list when the clear button is pressed
-        clear.setOnClickListener {
-
-            itemlist.clear()
-            adapter.notifyDataSetChanged()
-        }
-        // Adding the toast message to the list when an item on the list is pressed
-        listView.setOnItemClickListener { adapterView, view, i, l ->
-            android.widget.Toast.makeText(this, "You Selected the item --> "+itemlist.get(i), android.widget.Toast.LENGTH_SHORT).show()
-        }
-        // Selecting and Deleting the items from the list when the delete button is pressed
-        delete.setOnClickListener {
-            val position: SparseBooleanArray = listView.checkedItemPositions
-            val count = listView.count
-            var item = count - 1
-            while (item >= 0) {
-                if (position.get(item))
-                {
-                    adapter.remove(itemlist.get(item))
-                }
-                item--
-            }
-            position.clear()
-            adapter.notifyDataSetChanged()
-        }
-    }
-
-
 
     private fun updateUI(user: FirebaseUser?) {
 
